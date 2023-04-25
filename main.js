7096194f31a5db36ebeb324fe7bc43357fa8c5a4
Akuma-Tekina <Deathsynite@gmail.com>
2023-04-25 19:41:07 +0530

    added save and clear function

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const accountEl = document.getElementById("account-el")
 const riskEl = document.getElementById("risk-el")
 const pipsEl = document.getElementById("pips-el")
-function save() {
-    saved1 = accountEl.value
-    saved2 = riskEl.value
+window.onload = function() {
+    let savedValue1 = localStorage.getItem("myInputValue");
+    let savedValue2 = localStorage.getItem("myInputValue2");
+    if (savedValue1) {
+      accountEl.value = savedValue1;
+    }
+    if (savedValue2) {
+      riskEl.value = savedValue2;
+    }
+  };
+  function save() {
+    let saved1 = accountEl.value;
+    let saved2 = riskEl.value;
     localStorage.setItem("myInputValue", saved1);
     localStorage.setItem("myInputValue2", saved2);
-}
+  }
+
+  function clear() {
+    accountEl.value = "";
+    riskEl.value = "";
+    localStorage.removeItem("myInputValue");
+    localStorage.removeItem("myInputValue2");
+  }
